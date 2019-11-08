0af4f9f1f9b0a95c84af0ad9549fe800970ee3b2
Tiffceet <zkang.loo@gmail.com>
2019-11-08 21:15:11 +0800

    Templating possible functions for future use

--- Sports-Facilities-Management-System/booking.c
+++ Sports-Facilities-Management-System/booking.c
@@ -1,6 +1,35 @@
 #include "booking.h"
 #include "stdcxx.h"
-int bookingMenu() 
+
+void bookingSeachRecords()
+{
+
+}
+
+void bookingBook()
+{
+
+}
+
+void bookingModifyRecords()
+{
+
+}
+
+void bookingDisplayAll()
+{
+	FILE *f;
+	f = fopen(bookingFilePath, 'r');
+	if (!f)
+	{
+		printf("Missing booking.txt.\nIts all developers' fault. Contact the developers.\n");
+		fclose(f);
+		return;
+	}
+	fclose(f);
+}
+
+int bookingMenu()
 {
 	printf("==============\n");
 	printf("Booking Module\n");
@@ -9,6 +38,7 @@ int bookingMenu()
 	printf("2. Search records\n");
 	printf("3. Modify records\n");
 	printf("4. Display all bookings\n");
+	printf("5. Return to console\n");
 	printf("\nMenu Choice: ");
 	char choice[10];
 	if (getUserMenuChoice(choice, 9))
@@ -25,18 +55,34 @@ int bookingMenu()
 		break;
 	case '4':
 		break;
+	case '5':
+		return 0;
 	default:
 		return 1;
 	}
-	return 0;
+	return 1;
 
+}
+
+void bookingMakeSureFileExist()
+{
+	FILE *f = fopen(bookingFilePath, "r");
+	if (!f)
+	{
+		fclose(f);
+		f = fopen(bookingFilePath, "w");
+		fclose(f);
+	}
 }
 
 void bookingMain()
 {
+	// define filepath
+	sprintf(bookingFilePath, "%s\\%s", APPDATA_PATH, "booking.txt");
+	bookingMakeSureFileExist();
+	// while menu() doesnt return 0 = continue running
 	while (bookingMenu())
 	{
-
 		continue;
 	}
 }
--- Sports-Facilities-Management-System/booking.h
+++ Sports-Facilities-Management-System/booking.h
@@ -2,4 +2,10 @@
 #define BOOKING_H
 int bookingMenu();
 void bookingMain();
+void bookingDisplayAll();
+void bookingSeachRecords();
+void bookingBook();
+void bookingModifyRecords();
+char bookingFilePath[1024];
+
 #endif
