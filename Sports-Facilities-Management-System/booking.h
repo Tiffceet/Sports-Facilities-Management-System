92ff73ef22c44912c445aca789edac7ba6ae2676
Tiffceet <zkang.loo@gmail.com>
2019-11-23 23:56:35 +0800

    bookingBook() update <incomplete>

--- Sports-Facilities-Management-System/booking.h
+++ Sports-Facilities-Management-System/booking.h
@@ -1,10 +1,20 @@
 #ifndef BOOKING_H
 #define BOOKING_H
-int bookingMenu();
+
+// Main functions
 void bookingMain();
-void bookingDisplayAll();
-void bookingSeachRecords();
+int bookingMenu();
+void printBookingInfo();
 void bookingBook();
+void bookingSeachRecords();
 void bookingModifyRecords();
+void bookingDisplayAll();
+
+// Util Functions
+void incrementBookingID(char *oldStaffID);
+void askUserForBookingDate(Date *userPickedDate);
+void checkForTimeslotsAvailablity(int *timeslot, BookingData *data, int dataSize, Date *bookingDate, char *facilityID);
+// Global variable
 char bookingFilePath[1024];
+int err; // to keep track of input errors
 #endif
